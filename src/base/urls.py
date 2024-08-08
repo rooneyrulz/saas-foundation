da05ff5c24c511e07c702491987a80f8aae0eb47
ruzny.rulzz <ruzny.rulzz@gmail.com>
2024-08-08 13:15:16 +0530

    urls and dockerfile updated

--- src/base/urls.py
+++ src/base/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from .views import home_page_view, about_page_view
 
 urlpatterns = [
-    path("/", home_page_view),
+    path("", home_page_view),
     path("about/", about_page_view),
     path('admin/', admin.site.urls),
 ]
